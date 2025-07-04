0a61cf7a2a311a34619955de29dcde118a7a5f00
Ricardo <ricardohsmello@gmail.com>
2025-07-03 21:56:36 -0300

    Added option to find next open round

---
+++ application/src/main/java/br/com/g12/usecase/round/FindNextOpenRoundUseCase.java
@@ -0,0 +1,28 @@
+package br.com.g12.usecase.round;
+
+import br.com.g12.exception.RoundSummaryException;
+import br.com.g12.port.MatchPort;
+import br.com.g12.usecase.AbstractUseCase;
+
+public class FindNextOpenRoundUseCase extends AbstractUseCase<String> {
+
+    private final MatchPort matchPort;
+
+    public FindNextOpenRoundUseCase(final MatchPort matchPort) {
+        this.matchPort = matchPort;
+    }
+
+    public int execute() {
+        logInput("Finding round");
+
+        try {
+            return matchPort.findNextOpenRound();
+        } catch (RoundSummaryException e) {
+            logError(e);
+            throw e;
+        } finally {
+            logSuccess();
+        }
+
+    }
+}
