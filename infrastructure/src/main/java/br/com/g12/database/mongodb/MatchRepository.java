b40261bc6b89d556a842b9d4f82f2371319b5397
Ricardo <ricardohsmello@gmail.com>
2025-07-03 21:57:06 -0300

    Added option to find next open round

--- infrastructure/src/main/java/br/com/g12/database/mongodb/MatchRepository.java
+++ infrastructure/src/main/java/br/com/g12/database/mongodb/MatchRepository.java
@@ -1,13 +1,23 @@
 package br.com.g12.database.mongodb;
 
 import br.com.g12.entity.MatchDocument;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MatchRepository extends MongoRepository<MatchDocument, String> {
 
     List<MatchDocument> findByRound(int round);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'status': 'OPEN' } }",
+            "{ '$group': { '_id': '$round'} }",
+            "{ '$sort': { '_id': 1 } }",
+            "{ '$limit': 1  }"
+    })
+    Optional<Integer> findNextOpenRound();
 }
