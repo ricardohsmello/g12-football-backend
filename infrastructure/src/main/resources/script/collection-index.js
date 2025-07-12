de13de80290ad3ef2b14a73bdc4c47b030159cc7
Ricardo <ricardohsmello@gmail.com>
2025-07-12 12:19:28 -0300

    Added indexes file to control

---
+++ infrastructure/src/main/resources/script/collection-index.js
@@ -0,0 +1,32 @@
+db.bet.createIndex(
+  { matchId: 1, username: 1 },
+  { name: "matchIdAndUsername" }
+)
+
+db.bet.createIndex(
+  { matchId: 1 },
+  { name: "matchId_1" }
+)
+db.bet.getIndexes()
+
+
+db.match.createIndex(
+  { status: 1, matchDate: 1 },
+  { name: "statusAndDate" }
+)
+
+db.match.createIndex(
+  { round: 1 },
+  { name: "round_1" }
+)
+
+Índice composto em round + username
+db.scoreboard.createIndex(
+  { round: 1, username: 1 },
+  { name: "roundAndUsername" }
+)
+
+db.scoreboard.createIndex(
+  { round: 1 },
+  { name: "round_1" }
+)
