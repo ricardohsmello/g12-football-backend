eed5d8abfb3d1f6e22789ff5ece52611b281f921
Ricardo <ricardohsmello@gmail.com>
2025-07-18 02:19:52 -0300

    Including script to insert matches

---
+++ infrastructure/src/main/resources/script/match-insert.js
@@ -0,0 +1,83 @@
+/*
+const round = 15;
+db.match.insertMany(
+    [
+        // 19-07
+        {
+            "round": round,
+            "homeTeam": "Fortaleza",
+            "awayTeam": "Bahia",
+            "matchDate": new Date("2025-07-19T16:00:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "Vasco",
+            "awayTeam": "Grêmio",
+            "matchDate": new Date("2025-07-19T17:30:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "Mirassol",
+            "awayTeam": "Santos",
+            "matchDate": new Date("2025-07-19T18:30:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "São Paulo",
+            "awayTeam": "Corinthians",
+            "matchDate": new Date("2025-07-19T21:00:00-03:00"),
+            "status": "OPEN",
+        },
+
+        // 20-07
+
+        {
+            "round": round,
+            "homeTeam": "Internacional",
+            "awayTeam": "Ceará",
+            "matchDate": new Date("2025-07-20T11:00:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "Cruzeiro",
+            "awayTeam": "Juventude",
+            "matchDate": new Date("2025-07-20T16:00:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "Vitória",
+            "awayTeam": "Bragantino",
+            "matchDate": new Date("2025-07-20T16:00:00-03:00"),
+            "status": "OPEN",
+        },
+        {
+            "round": round,
+            "homeTeam": "Palmeiras",
+            "awayTeam": "Atlético Mineiro",
+            "matchDate": new Date("2025-07-20T17:30:00-03:00"),
+            "status": "OPEN",
+        },
+         {
+            "round": round,
+            "homeTeam": "Sport",
+            "awayTeam": "Botafogo",
+            "matchDate": new Date("2025-07-20T17:30:00-03:00"),
+            "status": "OPEN",
+        },
+              {
+            "round": round,
+            "homeTeam": "Flamengo",
+            "awayTeam": "Fluminense",
+            "matchDate": new Date("2025-07-20T19:30:00-03:00"),
+            "status": "OPEN",
+        },
+
+    ]
+)
+
+ */
