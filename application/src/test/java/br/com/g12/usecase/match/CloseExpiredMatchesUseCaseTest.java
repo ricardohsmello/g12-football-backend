d750c14eb663da3b119f1e3ad2e6d4e1215ae54b
Ricardo <ricardohsmello@gmail.com>
2025-07-24 18:01:00 -0300

    Including test for close expired matches

---
+++ application/src/test/java/br/com/g12/usecase/match/CloseExpiredMatchesUseCaseTest.java
@@ -0,0 +1,28 @@
+package br.com.g12.usecase.match;
+
+import br.com.g12.port.MatchPort;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class CloseExpiredMatchesUseCaseTest {
+
+    private final MatchPort matchPort = mock(MatchPort.class);
+    private final CloseExpiredMatchesUseCase useCase = new CloseExpiredMatchesUseCase(matchPort);
+
+    @Test
+    public void should_execute_schedule_successfully() {
+
+        when(matchPort.closeExpiredMatches(any(Date.class))).thenReturn(5);
+
+        int result = useCase.execute();
+
+        assertEquals(5, result);
+        verify(matchPort).closeExpiredMatches(any(Date.class));
+
+    }
+}
