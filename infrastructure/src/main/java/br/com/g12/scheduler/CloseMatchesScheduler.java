3728cfd2771f9922e8c8a840eab337f3c167dfd4
Ricardo <ricardohsmello@gmail.com>
2026-06-05 17:58:35 -0300

    Adjusting cron to execute in more specific time for wc

--- infrastructure/src/main/java/br/com/g12/scheduler/CloseMatchesScheduler.java
+++ infrastructure/src/main/java/br/com/g12/scheduler/CloseMatchesScheduler.java
@@ -17,9 +17,14 @@ public class CloseMatchesScheduler {
         this.closeExpiredMatchesUseCase = useCase;
     }
 
-    @Scheduled(cron = "0 0,30 11,16,17,18,19,20,21,22 * * *", zone = "America/Sao_Paulo")
+    @Scheduled(
+            cron = "0 0,30 0,1,11,13,14,16,17,18,19,20,21,22,23 * * *",
+            zone = "America/Sao_Paulo"
+    )
     public void run() {
         int closed = closeExpiredMatchesUseCase.execute();
         log.info("Closing Matches: {}", closed);
     }
+
+
 }
